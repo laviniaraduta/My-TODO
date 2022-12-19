e875adcfe98f24d55ef6b8db6ad75e877c27db14
laviniaraduta <lavinia.maria.raduta07@gmail.com>
2022-12-19 03:22:23 +0200

    Added FAB to add tasks

--- app/src/main/java/com/example/mytodo/tasks/TaskAdapter.kt
+++ app/src/main/java/com/example/mytodo/tasks/TaskAdapter.kt
@@ -1,7 +1,5 @@
 package com.example.mytodo.tasks
 
-import android.content.Context
-import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
