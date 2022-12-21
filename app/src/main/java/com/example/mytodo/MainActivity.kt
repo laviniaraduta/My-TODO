6aa8ea0215c367ddcbf64eaa3dfe1a863e7e45ad
laviniaraduta <lavinia.maria.raduta07@gmail.com>
2022-12-22 01:54:15 +0200

    added user photo using glide

--- app/src/main/java/com/example/mytodo/MainActivity.kt
+++ app/src/main/java/com/example/mytodo/MainActivity.kt
@@ -4,20 +4,23 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.bumptech.glide.Glide
 import com.example.mytodo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
     private lateinit var drawerLayout: DrawerLayout
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         drawerLayout = binding.drawerLayout
         val navController = this.findNavController(R.id.myNavHostFragment)
         NavigationUI.setupActionBarWithNavController(this,navController, drawerLayout)
@@ -36,6 +39,14 @@ class MainActivity : AppCompatActivity() {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
             editor?.putBoolean("switchValue", false)?.apply()
         }
+        val navHeader= binding.navView.getHeaderView(0)
+        val imageView = navHeader.findViewById<ImageView>(R.id.navHeaderImage)
+        Glide.with(this)
+            .load("https://lh3.googleusercontent.com/ogw/AOh-ky1XPZ9jP2HAjTWsEeyWgPZrkSkrRzxoN0coX4R3fg=s64-c-mo")
+            .placeholder(R.drawable.app_logo_foreground)
+            .fitCenter()
+            .into(imageView)
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
