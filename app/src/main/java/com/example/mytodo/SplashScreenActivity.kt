419c7fe6e0a3458be8b5814a67d148f0e74254e5
laviniaraduta <lavinia.maria.raduta07@gmail.com>
2022-12-21 02:12:13 +0200

    made theme change work

--- app/src/main/java/com/example/mytodo/SplashScreenActivity.kt
+++ app/src/main/java/com/example/mytodo/SplashScreenActivity.kt
@@ -1,16 +1,35 @@
 package com.example.mytodo
 
+import android.content.Context
 import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
 
 class SplashScreenActivity : AppCompatActivity() {
-
+    private val TAG = "SplashScreenActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val sharedPref = getPreferences(Context.MODE_PRIVATE)
+        val editor = sharedPref?.edit()
+
+        val state = sharedPref?.getBoolean("switchValue", false)
+        if (state == true) {
+            Log.d(TAG, "Dark mode is activated ")
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            editor?.putBoolean("switchValue", true)?.apply()
+        } else {
+            Log.d(TAG, "Light mode is activated ")
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            editor?.putBoolean("switchValue", false)?.apply()
+        }
+
+
+
         setContentView(R.layout.activity_splash_screen)
 
         val content = findViewById<View>(android.R.id.content)
@@ -22,5 +41,7 @@ class SplashScreenActivity : AppCompatActivity() {
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }, 2000) // 2 seconds
+
+
     }
 }
--- app/src/main/java/com/example/mytodo/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/mytodo/fragments/SettingsFragment.kt
@@ -1,6 +1,8 @@
 package com.example.mytodo.fragments
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -23,16 +25,20 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_settings, container, false)
+        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
+        val editor = sharedPref?.edit()
+        val state = sharedPref?.getBoolean("switchValue", false)
+        Log.d(TAG, "onCreateView: switchul are valoarea $state")
+        binding.switch1.isChecked = state?: false
         binding.switch1.setOnClickListener {
-            val state = binding.switch1.isActivated
-            if (state) {
+            if (binding.switch1.isChecked) {
+                Log.d(TAG, "activate Night mode")
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-//                activity?.setTheme(R.style.DarkTheme)
-//                activity?.recreate()
+                editor?.putBoolean("switchValue", true)?.apply()
             } else {
+                Log.d(TAG, "activate Light mode")
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-//                activity?.setTheme(R.style.LightTheme)
-//                activity?.recreate()
+                editor?.putBoolean("switchValue", false)?.apply()
             }
         }
         return binding.root
